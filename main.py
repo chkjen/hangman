5f9b71ab9e15c7f6e8d72c3c99b67e06a67e8c97
chkjen <60936694+chkjen@users.noreply.github.com>
2022-07-21 14:51:56 -0500

    iteration changes to add functionality

--- main.py
+++ main.py
@@ -12,10 +12,32 @@ def get_valid_word(words):
 
 def hangman():
     word = get_valid_word(words)
-    word_letter = set(word) # letters in the word
+    word_letters = set(word) # letters in the word
     alphabet = set(string.ascii_uppercase)
-    used_letters = set() # what letters have been guessed
+    user_letters = set() # what letters have been guessed
+
+    # user input for guesses
+    while len(word_letters) > 0:
+
+        # letters used
+        print("You have used the following letters: ", " ".join(used_letters))
+
+        # current word displayed as W - R D with spaces and dashes
+        word_list = [letter if letter in used_letters else "-" for letter in word]
+        print ("Current word: "," ".join(word_list))
+
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+        elif user_letter in used_letters:
+            print("You have already used that letter. Please try again")
+
+        else:
+            print("Invalid character")
+
+    # reaches this point when the length of word_letters == 0
 
 
-user_input = input("Guess a letter: ").upper()
-print(user_input)
+hangman()
