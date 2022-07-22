216ef740481751b1b1b3098324874f9d7795c348
chkjen <60936694+chkjen@users.noreply.github.com>
2022-07-22 13:01:46 -0500

    issue was you can't check for blank spaces in a while loop using the or and only using ' '. you need ot use " "

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import string
 
 def get_valid_word(words):
     word = random.choice(words) #chooses a random word from the list
-    #while '-' in word or ' ' in word:
-        #word = random.choice(words)
+    while '-' in word or " " in word:
+        word = random.choice(words)
 
     return word.upper()
 
